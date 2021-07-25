c06b7cda0f0d36908d56dac0e21f86b332694d7d
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2021-07-25 21:10:28 +1000

    Refactor unit test with grpc testing

--- services/hello/server/server_test.go
+++ services/hello/server/server_test.go
@@ -3,13 +3,61 @@ package server
 import (
 	"context"
 	"fmt"
+	"log"
+	"net"
 	"testing"
+	
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/test/bufconn"
 
 	"github.com/stretchr/testify/require"
+
 	pbh "github.com/terrencemiao/golang/protos/hello"
 )
 
-func TestHello(t *testing.T) {
+const bufSize = 1024 * 1024
+
+var lis *bufconn.Listener
+
+func init() {
+    lis = bufconn.Listen(bufSize)
+    s := grpc.NewServer()
+    pbh.RegisterHelloServer(s, &Server{})
+	
+    go func() {
+        if err := s.Serve(lis); err != nil {
+            log.Fatalf("Server exited with error: %v", err)
+        }
+    }()
+}
+
+func bufDialer(context.Context, string) (net.Conn, error) {
+    return lis.Dial()
+}
+
+func TestGreetGrpc(t *testing.T) {
+    ctx := context.Background()
+ 
+	conn, err := grpc.DialContext(ctx, "bufnet", grpc.WithContextDialer(bufDialer), grpc.WithInsecure())
+    
+	if err != nil {
+        t.Fatalf("Failed to dial bufnet: %v", err)
+    }
+    
+	defer conn.Close()
+    
+	client := pbh.NewHelloClient(conn)
+    resp, err := client.Greet(ctx, &pbh.GreetingRequest{Name: "Dr. Seuss"})
+    
+	if err != nil {
+        t.Fatalf("SayHello failed: %v", err)
+    }
+    
+	log.Printf(resp.Greeting)
+	fmt.Println(resp.Greeting)
+}
+
+func TestGreetNoGrpc(t *testing.T) {
 	srv := Server{}
 
 	parameters := []struct{
@@ -35,10 +83,13 @@ func TestHello(t *testing.T) {
 			t.Errorf("Greet(%v) got unexpected error", parameter.name)
 		}
 		
-		fmt.Println(resp.Greeting)
 		require.EqualValues(t, parameter.want, resp.Greeting)
+
 		if parameter.want != resp.Greeting {
 			t.Errorf("Great(%v) = %v, wanted %v", parameter.name, resp.Greeting, parameter.want)
 		}
+
+		log.Printf(resp.Greeting)
+		fmt.Println(resp.Greeting)
 	}
 }
