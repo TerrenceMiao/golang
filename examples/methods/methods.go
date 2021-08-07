b6518b4919866b52d4fd58d66de0f30d0c4f7bbb
Terrence Miao <TerrenceMiao@users.noreply.github.com>
2021-08-07 21:01:54 +1000

    Add 'method set' example

---
+++ examples/methods/methods.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+// A ethod set example
+func main() {
+	v := Vertex{3, 4}
+	fmt.Println(v.Abs())
+}
